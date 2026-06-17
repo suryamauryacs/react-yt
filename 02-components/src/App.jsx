59f7603e8f1e10d3c06b33b14894b45c4fb8eaf1
surya maurya <suryamauryacs@gmail.com>
2026-06-18 01:48:32 +0530

    update

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>Surya Maurya</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eveniet.</p>
+
+      </div>
+
+      <div className='card'>
+        <h1>Surya Maurya</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eveniet.</p>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
