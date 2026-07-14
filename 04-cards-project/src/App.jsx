2b90504b211286252f54d2b5b5034efa6e767596
surya maurya <suryamauryacs@gmail.com>
2026-07-14 14:12:40 +0530

    day4

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
         <div className="center">
           <h3>Amazon <span> 5 days ago</span></h3>
           <h2>Senior UI/UX Designer</h2>
-          <div>
+          <div className = 'tag'>
             <h4>Part Time</h4>
             <h4>Senior Level</h4>
           </div>
