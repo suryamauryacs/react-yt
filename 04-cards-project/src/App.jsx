4c6c4777a5de98181a2796c70af0e408ad5d56b4
surya maurya <suryamauryacs@gmail.com>
2026-07-24 10:20:08 +0530

    update

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Card from './components/Card'
+import User from './components/User'
+
+const arr = [10, 20, 30, 40];
 
 const App = () => {
   return (
     <div className='parent'>
-      <Card />
+      {/* <Card /> */}
+      {/* <User name="surya" />
+      <User name="Saurabh" /> */}
+
+
+      {arr.map(function () {
+        return 'hello'
+      })}
     </div>
 
   )
