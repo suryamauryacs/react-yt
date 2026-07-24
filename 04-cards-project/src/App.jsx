9949b279e8cb51e66590f322a6f5156556db0767
surya maurya <suryamauryacs@gmail.com>
2026-07-24 12:09:50 +0530

    update

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -2,7 +2,139 @@ import React from 'react'
 import Card from './components/Card'
 import User from './components/User'
 
-const arr = [10, 20, 30, 40];
+// const arr = [10, 20, 30, 40];
+const arr = [
+  {
+    user: 'surya',
+    age: 27
+  },
+  {
+    user: 'Saurabh',
+    age: 26
+  },
+  {
+    user: 'Shreya',
+    age: 25
+  }
+];
+
+
+arr.map(function (ele) {
+  console.log(ele.user);
+})
+
+const obj = {
+  user: 'surya',
+  age: 27
+}
+
+console.log(obj);
+
+const jobOpenings = [
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg",
+    companyName: "Google",
+    postedDate: "3 days ago",
+    post: "Software Engineer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$45/hr",
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+    companyName: "Microsoft",
+    postedDate: "1 week ago",
+    post: "Frontend Developer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$42/hr",
+    location: "Noida, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+    companyName: "Amazon",
+    postedDate: "5 days ago",
+    post: "Cloud Support Engineer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: "$38/hr",
+    location: "Hyderabad, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/0/05/Meta_Platforms_Inc._logo.svg",
+    companyName: "Meta",
+    postedDate: "2 weeks ago",
+    post: "React Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$65/hr",
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+    companyName: "Apple",
+    postedDate: "4 days ago",
+    post: "iOS Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$70/hr",
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/7/75/Netflix_icon.svg",
+    companyName: "Netflix",
+    postedDate: "10 days ago",
+    post: "Backend Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$75/hr",
+    location: "Delhi, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/sco/2/21/Nvidia_logo.svg",
+    companyName: "NVIDIA",
+    postedDate: "6 days ago",
+    post: "AI/ML Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$68/hr",
+    location: "Pune, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Adobe_Systems_logo_and_wordmark.svg",
+    companyName: "Adobe",
+    postedDate: "3 weeks ago",
+    post: "UI/UX Engineer",
+    tag1: "Part Time",
+    tag2: "Junior Level",
+    pay: "$35/hr",
+    location: "Noida, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png",
+    companyName: "Uber",
+    postedDate: "2 days ago",
+    post: "Java Developer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: "$50/hr",
+    location: "Delhi, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/5/50/Oracle_logo.svg",
+    companyName: "Oracle",
+    postedDate: "1 month ago",
+    post: "Database Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: "$58/hr",
+    location: "Mumbai, India"
+  }
+];
+
+console.log(jobOpenings);
+
 
 const App = () => {
   return (
@@ -11,10 +143,18 @@ const App = () => {
       {/* <User name="surya" />
       <User name="Saurabh" /> */}
 
+      {/* 
+      {arr.map(function (ele) {
+        return <h1>{ele/2}</h1>
+      })} */}
 
-      {arr.map(function () {
-        return 'hello'
-      })}
+      {
+        jobOpenings.map(function (elem) {
+          return <h1>{elem}</h1>
+        })
+      }
+
+
     </div>
 
   )
